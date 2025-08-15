d4b95d404fd96f812b3b4befd5aac343c94fbcd7
vinimello90 <vinicius.barretto9022@gmail.com>
2025-08-15 12:07:01 -0300

    feat(main): add auth section html structure

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  template: ` <main></main> `,
+  template: `
+    <main>
+      <section class="auth">
+        <div class="auth__container">
+          <form action="" class="auth__form">
+            <label class="auth__label">
+              <input type="text" class="auth__input" placeholder="E-mail or username" required />
+            </label>
+            <button type="submit" class="auth__button">Register Passkey</button>
+            <button type="submit" class="auth__button">Authenticate</button>
+          </form>
+        </div>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent {}
