4701ba6345212e221bd3b6a74813e79c406b185d
vinimello90 <vinicius.barretto9022@gmail.com>
2025-08-16 08:39:26 -0300

    refactor(templates): convert inline templates to use templateUrl

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,22 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-    <header class="header">
-      <div class="header__top">
-        <img src="../../../assets/logo.svg" alt="logo" class="header__logo" />
-        <div class="header__container-title">
-          <h1 class="header__title">Web Authentication</h1>
-          <h2 class="header__sub-title">Secure & Password-Free Login</h2>
-        </div>
-      </div>
-      <p class="header__description">
-        This project demonstrates modern, secure authentication using WebAuthn. Users can log in
-        passwordlessly, providing a faster, safer, and more seamless experience while eliminating
-        the risks associated with traditional passwords.
-      </p>
-    </header>
-  `,
+  templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,12 +3,9 @@ import { AuthComponent } from './components/auth/auth.component';
 
 @Component({
   selector: 'app-main',
+
   imports: [AuthComponent],
-  template: `
-    <main class="main">
-      <app-auth />
-    </main>
-  `,
+  templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent {}
