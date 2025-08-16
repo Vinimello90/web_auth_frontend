13f19dc5b583903a909de92d4bde1d815b588da7
vinimello90 <vinicius.barretto9022@gmail.com>
2025-08-16 18:17:02 -0300

    feat(auth): add reactive form for user input

--- src/app/components/main/components/auth/auth.component.ts
+++ src/app/components/main/components/auth/auth.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  userForm = new FormGroup({
+    user: new FormControl(''),
+  });
+
+  onRegister() {
+    console.log({ register: this.userForm.value.user });
+  }
+
+  onAuthenticate() {
+    console.log({ authenticate: this.userForm.value.user });
+  }
+}
