db926dad3a399580f71feab54da0ba8daa88319c
vinimello90 <vinicius.barretto9022@gmail.com>
2025-08-28 10:56:13 -0300

    fix(auth): auth component test

--- src/app/guards/guest-guard.spec.ts
+++ src/app/guards/guest-guard.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { guestGuard } from '../guest-guard';
+import { guestGuard } from './guest-guard';
 
 describe('guestGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
